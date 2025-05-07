837b4e790d8878b82a094e04e2f99ea35b7c36e5
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-05-07 21:21:37 +0200

    Create function that takes the data from the fetch call and  returns the weather data of choice

---
+++ src/returnedWeatherData.js
@@ -0,0 +1,21 @@
+async function returnedData(data) {
+    if(!data) {
+        return `No weather data could be retrieved`;
+    }
+
+    const conditions = data.currentConditions.conditions;
+    const feelsLike = data.currentConditions.feelslike;
+    const humidity = data.currentConditions.humidity;
+    const dateTime = data.currentConditions.datetime;
+    const snow = data.currentConditions.snow;
+    const sunrise = data.currentConditions.sunrise;
+    const sunset = data.currentConditions.sunset;
+    const temperature = data.currentConditions.temp;
+    const visibility = data.currentConditions.visibility;
+    const windspeed = data.currentConditions.windspeed;
+    const daysArr = data.days;
+
+    return { conditions, feelsLike, humidity, dateTime, snow, sunrise, sunset, temperature, visibility, windspeed, daysArr };
+}
+
+export { returnedData };
