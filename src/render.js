219db0454a2956e5b93474f838f8c934448e6333
AgA <161239277+Just-AgA@users.noreply.github.com>
2025-05-14 20:43:34 +0200

    Add HTML elements that the render function will generate

--- src/render.js
+++ src/render.js
@@ -17,5 +17,23 @@ async function renderCurrentWeather(data) {
     const feelsLikeF = data.feelsLike;
     const feelsLikeC = ((feelsLikeF - 32) * 5 / 9).toFixed(1);
 
-    
+    currConditions.innerHTML = `<h1>Current Conditions:</h1>
+                                <h2>
+                                    Temperature: 
+                                    <span class="temp-value" data-f="${tempF}" data-c="${tempC}" data-unit="F">${tempF}°F</span>,  Feels like: 
+                                    <span class="feels-like" data-f="${feelsLikeF}" data-c="${feelsLikeC}" data-unit="F">${feelsLikeF}°F</span>
+                                    <button class="toggle-temp">Toggle temperature</button><br>
+                                </h2>
+                                <h3>Time: ${data.dateTime}</h3>
+                                <h3>Conditions: ${data.conditions}</h3>
+                                <h3>Sunrise: ${data.sunrise} Sunset: ${data.sunset}</h3>
+                                <h3>Windspeed: ${data.windspeed}</h3>
+                                <h3>Visibility: ${data.visibility}</h3>
+                                <h4>Snow: ${data.snow}</h4>
+                                <h4>Max temp: ${((data.daysArr[0].tempmax - 32) * 5 / 9).toFixed(1)}°C</h4>
+                                <h4>Min temp: ${((data.daysArr[0].tempmin - 32) * 5 / 9).toFixed(1)}°C</h4>`;
+
+    currConditions.prepend(img);
+
+   
 }
